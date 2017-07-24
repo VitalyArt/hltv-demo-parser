d35d840eb88ecbd9fcb08d4cbef46c565139af85
Artemyev Vitaly <artemyev@retailcrm.ru>
2017-07-24 22:23:02 +0300

    Added parser

---
+++ DemoParser.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace VitalyArt;
+
+
+class DemoParser implements \ArrayAccess
+{
+    const BAD_FILE_EXTENSION = 10;
+    const BAD_FILE_FORMAT = 20;
+    const FILE_NOT_READY = 30;
+
+    private $data = [];
+
+    /**
+     * @param string $file
+     * @throws \Exception
+     */
+    function __construct($file)
+    {
+        if ($this->extOfFile($file) !== "dem") {
+            throw new \Exception('Bad file extension', static::BAD_FILE_EXTENSION);
+        }
+
+        $handle = fopen($file, "r");
+
+        if (!$handle) {
+            throw new \Exception('File not found or unable to read', static::FILE_NOT_READY);
+        }
+
+        if ($this->readData($handle, 0, 8) === "HLDEMO") {
+            throw new \Exception('Bad file format', static::BAD_FILE_FORMAT);
+        }
+
+        $entriesList = [];
+
+        $entriesOffset = $this->readInt($handle, 540);
+        $entriesCount = $this->readInt($handle, $entriesOffset);
+
+        for ($i = 0; $i < $entriesCount; $i++) {
+            if ($this->readData($handle, $entriesOffset + 96 * $i + 4, 64)) {
+                $key = trim($this->readData($handle, $entriesOffset + 96 * $i + 4, 64));
+                $key = mb_convert_case($key, MB_CASE_LOWER, "UTF-8");
+
+                $entry = [
+                    'nEntryType'    => $this->readInt($handle, $entriesOffset + 96 * $i),
+                    'szDescription' => $this->readData($handle, $entriesOffset + 96 * $i + 4, 64),
+                    'nFlags'        => $this->readInt($handle, $entriesOffset + 96 * $i + 68),
+                    'nCDTrack'      => $this->readInt($handle, $entriesOffset + 96 * $i + 72),
+                    'fTrackTime'    => $this->readFloat($handle, $entriesOffset + 96 * $i + 76),
+                    'nFrames'       => $this->readInt($handle, $entriesOffset + 96 * $i + 80),
+                    'nOffset'       => $this->readInt($handle, $entriesOffset + 96 * $i + 84),
+                    'nFileLength'   => $this->readInt($handle, $entriesOffset + 96 * $i + 88)
+                ];
+
+                if ($this->isValidEntry($entry)) {
+                    $entriesList[$key] = $entry;
+                }
+            }
+        }
+
+        $this->data = [
+            'demoProtocol' => $this->readInt($handle, 8),
+            'netProtocol'  => $this->readInt($handle, 12),
+            'mapName'      => $this->readData($handle, 16, 260),
+            'clientName'   => $this->readData($handle, 276, 260),
+            'entries'      => $entriesList
+        ];
+
+        fclose($handle);
+    }
+
+    private function isValidEntry($entry)
+    {
+        foreach ($entry as $value) {
+            if ($value === false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param resource $file
+     * @param integer $offset
+     * @return bool
+     */
+    private function readInt($file, $offset)
+    {
+        if (fseek($file, $offset) == -1) {
+            return false;
+        }
+
+        $data = unpack("i", fread($file, 4));
+        return $data[1];
+    }
+
+    /**
+     * @param resource $file
+     * @param integer $offset
+     * @return bool
+     */
+    private function readUint($file, $offset)
+    {
+        if (fseek($file, $offset) == -1) {
+            return false;
+        }
+
+        $data = unpack("I", fread($file, 4));
+        return $data[1];
+    }
+
+    /**
+     * @param resource $file
+     * @param integer $offset
+     * @return bool
+     */
+    private function readFloat($file, $offset)
+    {
+        if (fseek($file, $offset) == -1) {
+            return false;
+        }
+
+        $data = unpack("f", fread($file, 4));
+        return $data[1];
+    }
+
+    /**
+     * @param resource $file
+     * @param integer $offset
+     * @param integer $Len
+     * @return string
+     */
+    private function readData($file, $offset, $Len)
+    {
+        if (fseek($file, $offset) == -1) {
+            return false;
+        }
+
+        return trim(fread($file, $Len));
+    }
+
+    /**
+     * @param string $file
+     * @return string
+     */
+    private function extOfFile($file)
+    {
+        $path = explode('.', $file);
+        return mb_convert_case(end($path), MB_CASE_LOWER);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->data[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->data[] = $value;
+        } else {
+            $this->data[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->data[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
+    }
+}
